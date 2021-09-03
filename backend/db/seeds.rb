1231f55d949cbf2803fb9c738aa63f1bc755f1ec
難波智貴 <nanbatomoki@nanbasatoshitakanoMacBook-Pro.local>
2021-09-04 00:23:31 +0900

    finish todo

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating todos...'
+
+# 適当なTodoを5つ作成
+5.times do |i|
+  Todo.create(title: "Todo#{i + 1}")
+end
+
+puts '...Finished!'
